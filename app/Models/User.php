879e6f4a2a585062fcbd263772ddcb06346e7973
Daksh Joshi <dakshjoshi1355@gmail.com>
2026-03-05 23:41:51 +0530

    Models

--- app/Models/User.php
+++ app/Models/User.php
@@ -82,6 +82,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@example.com');
+        return str_ends_with($this->email, '@gmail.com');
     }
 }
