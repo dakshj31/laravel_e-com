cf50e6a71fab986255d056fb50ec0e0065ec9904
Daksh Joshi <dakshjoshi1355@gmail.com>
2026-04-18 23:42:32 +0530

    Dummy Data

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Attributes\Scope;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Customer extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'name',
         'email',
