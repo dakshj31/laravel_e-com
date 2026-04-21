69df306914d745c4f51963ffad82732926a45fa9
Daksh Joshi <dakshjoshi1355@gmail.com>
2026-04-21 21:58:09 +0530

    AUthentication

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,21 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+Route::middleware('auth:customer')->group(function() {
+    //logout
+    Route::post('/logout', function() {
+        Auth::guard('customer')->logout();
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+        return redirect('/');
+    })->name('logout');
+});
 
 
 Route::view('dashboard', 'dashboard')
