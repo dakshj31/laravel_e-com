6c030a62c5b4d2dc561a372d8838c45bc1af87d2
Daksh Joshi <dakshjoshi1355@gmail.com>
2026-04-27 23:41:58 +0530

    HomePage

--- app/Livewire/ProductListing.php
+++ app/Livewire/ProductListing.php
@@ -2,12 +2,83 @@
 
 namespace App\Livewire;
 
+use App\Models\Category;
+use App\Models\Product;
+use Livewire\Attributes\Url;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ProductListing extends Component
 {
+    use WithPagination;
+
+    #[Url]
+
+    public $category = '';
+
+    #[Url]
+
+    public $search = '';
+
+    #[Url]
+
+    public $brand = '';
+
+    #[Url]
+
+    public $minPrice = '';
+
+    #[Url]
+
+    public $maxPrice = '';
+
+    #[Url]
+
+    public $sort = 'newest';
+
+    #[Url]
+
+    public $featured = '';
+
+    public $priceRange = [0,10000];
+
+    public function mount() {
+        // set the price range based on availabe products
+        $maxProductPrice = Product::active()->max('price') ?? 10000;
+        $this->priceRange = [0, ceil($maxProductPrice)];
+
+        if(empty($this->maxPrice)) {
+            $this->maxPrice = $this->priceRange[1];
+        }
+    }
+
+
     public function render()
     {
+
+        $query = Product::query()
+        ->active()
+        ->with(['category', 'brand', 'primaryImage']);
+
+        //search
+        if($this->search) {
+            $query->where(function($q) {
+                $q->where('name', 'like', '%' . this->search . '%')
+                ->orWhere('description', 'like', '%' . this->search . '%')
+                ->orWhere('sku', 'like', '%' . this->search . '%');
+            });
+        }
+
+        //category
+        if($this->category) {
+            $categoryModel = Category::where('slug', $this->category)->first();
+            if($categoryModel) {
+                $query->where('category_id', $categoryModel->id);
+            }
+        }
+
+        $products = $query->paginate(12);
+        dd($products);
         return view('livewire.product-listing')->layout('layouts.front-end-layout');
     }
 }
